6123b6e1932590afb8d6cae338948db4c47d899f
3thernity <3thernitydev@gmail.com>
2024-11-19 10:55:01 +0400

    patch turbo error and add catalogController

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/catalog', name: 'catalog_')]
 class CatalogController extends AbstractController
 {
-    #[Route('/catalog', name: 'app_catalog')]
-    public function index(): Response
+    public function __construct(
+        private LoggerInterface $logger
+    ) {}
+    #[Route('/', name: 'list')]
+    public function list(): Response
     {
+        $this->logger->info('Visite du catalogue');
         return $this->render('catalog/index.html.twig', [
             'controller_name' => 'CatalogController',
+            'action_name' => 'list',
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(int $id): Response
+    {
+        $this->logger->info('Consultation produit', ['id' => $id]);
+        return $this->render('catalog/index.html.twig', [
+            'controller_name' => 'CatalogController',
+            'action_name' => 'show',
+            'product_id' => $id,
         ]);
     }
 }
